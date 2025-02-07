1d1c6ad6675e6ba19400d1d1d6d8278de1c1fa89
MatosRicardo <ricardoribeirodematos@gmail.com>
2025-02-07 16:06:43 -0300

    feat: logic game

---
+++ src/components/Player.jsx
@@ -0,0 +1,13 @@
+export default function Player({ name, symbol }) {
+  return (
+    <>
+      <li>
+        <span className="player">
+          <span className="player-name">{name}</span>
+          <span className="player-symbol">{symbol}</span>
+        </span>
+        <button>Editar</button>
+      </li>
+    </>
+  );
+}
